ef0eef9001a2fab46b36e9fb1c91a8847c1f6ebb
Евгений Нифантьев <nifantyev@gmail.com>
2022-01-30 17:43:18 +0300

    Реализовал Logout

---
+++ src/components/Logout.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import AuthContext from '../contexts/AuthContext';
+
+const Logout = ({ onLogout }) => {
+  const { profile } = useContext(AuthContext);
+
+  const logo = (
+    <div className="col">
+      <h2>Neto Social</h2>
+    </div>
+  );
+
+  const handleLogout = () => {
+    onLogout();
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row align-items-center">
+        {logo}
+        <div className="col-auto">Hello, {profile.name}</div>
+        <div className="col-auto">
+          <img
+            className="rounded-circle"
+            src={profile.avatar}
+            alt={profile.name}
+          />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-outline-danger" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Logout.propTypes = {
+  onLogout: PropTypes.func.isRequired,
+};
+
+export default Logout;
