4da2ada06b546c1d39203ca4b4585c6ead3d2a73
Евгений Нифантьев <nifantyev@gmail.com>
2022-01-30 17:46:46 +0300

    Реализовал News

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthProvider from './contexts/AuthProvider';
 import Landing from './pages/Landing';
+import News from './pages/News';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/news" element={<News />} />
         </Routes>
       </Router>
     </AuthProvider>
---
+++ src/pages/News.js
@@ -0,0 +1,99 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../contexts/AuthContext';
+import Logout from '../components/Logout';
+
+const NewsItem = ({ src, title, content }) => {
+  return (
+    <div className="card mb-4">
+      <img src={src} className="card-img-top" alt={title} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{content}</p>
+      </div>
+    </div>
+  );
+};
+
+const News = () => {
+  const { token, handleLogout } = useContext(AuthContext);
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      const fetchNews = async () => {
+        try {
+          setError(null);
+          setLoading(true);
+          const response = await fetch(
+            `${process.env.REACT_APP_BASE_API_URL}/private/news`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.ok) {
+            const json = await response.json();
+            setNews(json);
+            setLoading(false);
+          } else {
+            if (response.status === 401) {
+              handleLogout();
+              navigate('/');
+            }
+          }
+        } catch (e) {
+          setError(e.message);
+          setLoading(false);
+        }
+      };
+      fetchNews();
+    } else {
+      handleLogout();
+      navigate('/');
+    }
+  }, [token, handleLogout, navigate]);
+
+  const onLogout = () => {
+    handleLogout();
+    navigate('/');
+  };
+
+  return (
+    <>
+      {loading && (
+        <div className="container">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      )}
+      {token && news && (
+        <>
+          <Logout onLogout={onLogout} />
+          <div className="container">
+            {error && (
+              <div class="row">
+                <div class="col">{error}</div>
+              </div>
+            )}
+            <div className="row">
+              {news.map((o) => (
+                <div key={o.id} className="col-4">
+                  <NewsItem src={o.image} title={o.title} content={o.content} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default News;
